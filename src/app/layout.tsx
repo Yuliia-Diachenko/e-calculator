a66950eba1ff4aff0e09287a74d442cba8bdb825
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-10-11 10:21:21 +0300

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
+import logoheader from '@/public/logo-header.png';
 import React from 'react';
 import './globals.css';
 
@@ -11,12 +14,12 @@ export default function RootLayout({
     <html lang="uk">
       <body className='container mx-auto'>
         <header className="h-100 flex flex-wrap justify-between items-center bg-gray-250 mx-100">
-          <Link href="/" className='w-140 h-100'>ЛОГО</Link>
+          <Link href="/" className='pl-100'><Image src={logoheader} className='w-51 h-62' alt='logo'/></Link>
           <Link href="/poll">Опитування</Link>
         </header>
         {children}
         <footer className='flex '>
-          <Link href="/"><svg className='fill-blue-950'></svg></Link>
+          <Link href="/"><Image src={logo} className='w-107 h-129' alt='logo'/></Link>
           <Link href="/about">Про нас</Link>
           <Link href="/partners">Партнери</Link>
           <Link href="/contacts">Контакти</Link>
