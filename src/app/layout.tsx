dd8a01cc8bdcde7c79a0885e39fa3159ed9d8412
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-10-11 09:49:58 +0300

    add color in tailwind.config.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,13 @@ export default function RootLayout({
   return (
     <html lang="uk">
       <body className='container mx-auto'>
-        <header className="h-100 flex flex-wrap justify-between items-center bg-gray-300 mx-100">
+        <header className="h-100 flex flex-wrap justify-between items-center bg-gray-250 mx-100">
           <Link href="/" className='w-140 h-100'>ЛОГО</Link>
           <Link href="/poll">Опитування</Link>
         </header>
         {children}
-        <footer>
-          <Link href="/">ЛОГО</Link>
+        <footer className='flex '>
+          <Link href="/"><svg className='fill-blue-950'></svg></Link>
           <Link href="/about">Про нас</Link>
           <Link href="/partners">Партнери</Link>
           <Link href="/contacts">Контакти</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: { colors: {
+      gray: {
+        250:'#eeeeee',
+        550: '#d9d9d9',
+      },
+    }},
   },
   plugins: [],
 };
